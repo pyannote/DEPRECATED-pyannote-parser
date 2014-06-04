89a5b3d3ee724dfb5f3e4a5ad69414802467f895
Herve Bredin <bredin@limsi.fr>
2014-06-04 13:58:23 +0200

    chore: increment version of required pyannote.core

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     namespace_packages=['pyannote'],
     packages=find_packages(),
     install_requires=[
-        'pyannote.core >= 0.0.2',
+        'pyannote.core >= 0.0.3',
     ],
     # versioneer
     version=versioneer.get_version(),
