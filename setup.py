0fe4bc6f79143be4b0d92af2edd0be3a1ab4cd2a
Herve Bredin <bredin@limsi.fr>
2014-11-17 15:19:56 +0100

    setup: depends on pyannote.core 0.2.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     namespace_packages=['pyannote'],
     packages=find_packages(),
     install_requires=[
-        'pyannote.core >= 0.2.2',
+        'pyannote.core >= 0.2.3',
         'pysrt >= 1.0.1'
     ],
     # versioneer
